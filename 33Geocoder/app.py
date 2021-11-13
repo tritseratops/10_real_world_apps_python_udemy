736cc41d876875a3dad02590f9defbbb34cfad88
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2021-11-13 09:08:41 +0200

    Finished geocoding and started sec 34 bonus exercises

--- 33Geocoder/app.py
+++ 33Geocoder/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
-
+# from pandas import DataFrame, read_csv
+import pandas
+from addgeocodes import add_geocodes
+import datetime
 
 app = Flask(__name__)
 
@@ -8,32 +11,35 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/select_file")
-def select_file():
-    return render_template("index.html", btn="download.html")
-
-@app.route("/my_send_file", methods=['POST'])
-def my_send_file():
+@app.route("/show_table", methods=['POST'])
+def show_table():
+    print('Inside my send file')
     if request.method=='POST':
         file = request.files['file']
-        file.save(secure_filename(file.filename))
-        with open(file.filename, "a") as f:
-            f.write("This is added after upload.")
-        return render_template("index.html")
+        print("filename:"+file.filename)
+        try:
+            df = pandas.read_csv(file)
+            if 'Address' in df.columns or 'address' in df.columns:
+                geo_df = add_geocodes(df)
+                geo_filename = datetime.datetime.now().strftime("uploads/"+file.filename+"Y%-m%-d%-H%-M%-S%-f%"+".csv")
+                geo_df.to_csv(secure_filename(geo_filename), sep=',')
+                no_address_message= ""
+                geo_df.to_html(header="true", table_id="table")
+                return render_template('index.html', tables=[geo_df.to_html(classes='data')], titles=geo_df.columns.values, filename=geo_filename)
+            else:
+                # print that no address column found
+                no_address_message = "Seems like three is no address column in this file"
+                # text = "Seems like three is no address column in this file"
+                return render_template('index.html', text=no_address_message)
+        except:
+            no_address_message = "Seems like this file cant be parsed"
+            # text = "Seems like three is no address column in this file"
+            return render_template('index.html', text=no_address_message)
 
-@app.route("/success", methods=['POST'])
-def success():
-    if request.method=='POST':
-        email = request.form['email_name']
-        height =  request.form['height_name']
-        print(email + " " + height)
-        data = Data(email, height)
-        # print(db.session.query(Data).filter(Data.email_==email).count())
-        return render_template("index.html", text = "Seems like we 've got something from this email address already")
 
 @app.route("/download")
 def download():
-    return send_file("Sample.csv", attachment_filename="yourfile.csv", as_attachment=True)
+    return send_file("test.csv", attachment_filename="test.csv", as_attachment=True)
 
 if __name__ == '__main__':
     app.debug  = True
