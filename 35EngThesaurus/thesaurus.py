681d903606b011e5ffdfa024c75bda69be668cc1
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2021-11-20 12:01:55 +0200

    Finished English thesaurus with mysql, added architecture to select db or json on demand

--- 35EngThesaurus/thesaurus.py
+++ 35EngThesaurus/thesaurus.py
@@ -1,38 +1,46 @@
 import json
 import difflib
+from mysql_vocabulary import MySQLVocabulary
+from json_vocabulary import JSONVocabulary
 
-def get_description(word):
+
+
+
+
+def get_uni_description(word, vocabulary):
     word = word.lower()
-    f = open('data\\data.json')
-    vocabulary = json.load(f)
-    words = vocabulary.keys()
+    words = vocabulary.get_words()
     if not word in words:
         if word.capitalize() in words:
             word = word.capitalize()
         elif word.upper() in words:
             word = word.upper()
     if word in words:
-        if len(vocabulary[word])==1:
-            return vocabulary[word][0]
+        definition = vocabulary.get_definitions(word)
+        if len(definition) == 1:
+            return definition[0]
         else:
             # form listed output
             output = f'Possible meanings for {word}:\n'
             number = 1
-            print(vocabulary[word])
-            print("word:" + str(vocabulary[word][0]))
-
-            for match in vocabulary[word]:
+            # print(definition)
+            # print("word:" + str(definition))
+            for match in definition:
                 output = output + str(number) + ": " + match + "\n"
                 number += 1
             return output
     else:
         limit_ratio = 0.75
-        close_matches = difflib.get_close_matches(word,vocabulary, cutoff=limit_ratio)
+        close_matches = difflib.get_close_matches(word, words, cutoff=limit_ratio)
         output = f'Could not find word: {word} , did you mean:\n'
         number = 1
         for match in close_matches:
-            output=output+str(number)+": "+match+"\n"
-            number+=1
-        if len(close_matches)==0:
+            output = output + str(number) + ": " + match + "\n"
+            number += 1
+        if len(close_matches) == 0:
             output = f"Could not find similar words tot '{word}', please check your grammar"
         return output
+
+def get_description(word):
+    # return get_uni_description(word, JSONVocabulary())
+    return get_uni_description(word, MySQLVocabulary())
